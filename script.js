caf14a7da6a86dfa4a78354f159671704a8d0ee8
Ritesh kasaudhan <RITESHKASAUDHAN975@GMAIL.COM>
2024-08-02 08:42:51 +0530

    Add
    Create a game where you start with any random game number. 
    ask the user to keep guessing the game number until the user entered Correct value.

---
+++ script.js
@@ -0,0 +1,7 @@
+let gamenum=25;
+let usernum=prompt("Guess the game number");
+while(usernum !=gamenum)
+{
+    usernum=prompt("You entered worng number.Guess again:");
+}
+console.log("Congratulations,You Entered The Right Number😍😍😍👍👍");
